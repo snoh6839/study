7ffa021b76e139d52ea31c606db44a5d1d649696
SubinNoh <snoh6839@mysvc.skagit.edu>
2023-11-01 12:16:40 +0900

    first commit

---
+++ 자바파일뜯어보기 요약.java
@@ -0,0 +1,212 @@
+/*
+ * HTML TagFilter.java 파일 요약
+ * HTMLTagFilter 클래스는 Filter 인터페이스를 구성하고 있습니다.
+ * doFilter 메서드는 필터를 통해 요청을 처리하는 역할을 합니다.
+ * HTMLTagFilterRequestWrapper를 사용하여 요청을 필터링한 후 체인에 전달합니다.
+ * 이 메서드는 IOException과 ServletException을 처리할 수 있도록 예외를 던지고 있습니다.
+ * init 메서드는 필터를 초기화할 때 호출되는 메서드입니다.
+ * FilterConfig 객체를 매개변수로 받아 필요한 초기화 작업을 수행할 수 있습니다.
+ * destroy 메서드는 필터가 종료될 때 호출되는 메서드입니다.
+ * 필터에서 사용한 자원을 정리하거나 종료 작업을 수행할 수 있습니다.
+ */
+
+/*
+ * HTML TagFilterRequesWrapper.java 파일 요약
+ * HTMLTagFilterRequestWrapper 클래스는 HttpServletRequestWrapper를 상속하여,
+ * HttpServletRequest 인터페이스의 메서드를 재정의하거나 추가하여 사용자 정의 요청 처리를 구현합니다.
+ * getParameterValues 메서드와 getParameter 메서드는 요청 파라미터의 값들을 가져와
+ * HTML 특수 문자를 이스케이핑하는 역할을 합니다.
+ * 이를 통해 XSS(Cross Site Scripting) 공격으로부터 보호할 수 있습니다.
+ * 이스케이핑 작업은 문자열에 포함된 HTML 특수 문자를 해당하는 HTML 엔티티로 치환하는 과정입니다.
+ * 이 클래스는 HTML 태그 필터링 기능을 구현하는 필터 클래스 HTMLTagFilter에서 사용되며, 요청을 필터링하는 역할을 합니다
+ */
+
+/*
+ * EgovProperties.java 파일 주요설명
+ * HTMLTagFilterRequestWrapper 클래스는 HttpServletRequestWrapper를 상속하여,
+ * HttpServletRequest 인터페이스의 메서드를 재정의하거나 추가하여 사용자 정의 요청 처리를 구현합니다.
+ * getParameterValues 메서드와 getParameter 메서드는 요청 파라미터의 값들을 가져와 HTML 특수 문자를
+ * 이스케이핑하는 역할을 합니다.
+ * 이를 통해 XSS(Cross Site Scripting) 공격으로부터 보호할 수 있습니다.
+ * 이스케이핑 작업은 문자열에 포함된 HTML 특수 문자를 해당하는 HTML 엔티티로 치환하는 과정입니다.
+ * 이 클래스는 HTML 태그 필터링 기능을 구현하는 필터 클래스 HTMLTagFilter에서 사용되며, 요청을 필터링하는 역할을 합니다.
+ */
+
+/*
+ * Globals.java 파일 요약
+ * Globals 클래스는 서비스 전역에서 사용되는 상수 값을 관리하기 위한 클래스입니다.
+ * 각 변수는 EgovProperties.getProperty() 또는 EgovProperties.getPathProperty()를 통해
+ * 속성 파일에서 해당 값들을 가져와 초기화합니다. 이를 통해 속성 파일의 내용이 변경되더라도 코드의 수정 없이 동작을 변경할 수 있습니다.
+ * 이 클래스에서 관리하는 상수 값들은 파일 경로, OS 타입, DB 타입, 메인 페이지 URL 등 서비스의 설정 값이나 상태 값을
+ * 나타냅니다.
+ * 이 값들은 서비스의 동작을 제어하거나, 서비스의 상태를 나타내는 데 사용됩니다.
+ */
+
+/*
+ * EgovBasicLogger.java 파일 요약
+ * EgovBasicLogger 클래스는 로깅 기능을 제공하는 클래스입니다.
+ * 이 클래스를 통해 "ignore", "debug", "info" 세 가지 로그 레벨의 로그를 기록할 수 있습니다.
+ * ignore, debug, info 각 메서드는 해당 로그 레벨의 로그를 기록하는 역할을 합니다.
+ * 각 메서드는 메시지와 예외를 인수로 받아, 예외가 있는 경우 예외 정보와 함께 로그를 기록하고, 예외가 없는 경우 메시지만 로그로
+ * 기록합니다.
+ * Logger.getLogger() 메서드를 사용하여 각 로그 레벨에 대한 Logger 인스턴스를 생성하고 있습니다. 이 인스턴스를 통해
+ * 로그를 기록합니다.
+ * Logger 인스턴스는 로그 메시지를 필터링하고, 필요에 따라 로그 레코드를 생성하여 해당 레코드를 출력 핸들러로 전달하는 역할을 합니다.
+ * 이를 통해 로그의 출력 방식, 형식, 필터링 등을 유연하게 조절할 수 있습니다.
+ */
+
+/*
+ * EgovResourseCloseHelper.java 파일 요약
+ * EgovResourceCloseHelper 클래스는 리소스를 안전하게 종료하는 기능을 제공하는 클래스입니다.
+ * close 메서드는 Closeable 배열에 대한 리소스를 안전하게 종료하는 역할을 합니다.
+ * 각 리소스를 순회하면서 close 메서드를 호출하고, 예외가 발생해도 무시합니다.
+ * closeDBObjects 메서드는 Wrapper 배열에 대한 데이터베이스 객체를 안전하게 종료하는 역할을 합니다.
+ * 각 객체를 순회하면서 해당 객체의 타입을 확인하고, 해당 타입에 맞는 close 메서드를 호출하여 리소스를 종료합니다.
+ * closeSocketObjects 메서드는 Socket과 ServerSocket 객체를 안전하게 종료하는 역할을 합니다.
+ * socket 객체에 대해서는 shutdownOutput 메서드를 호출하고, close 메서드를 호출하여 리소스를 종료합니다.
+ * server 객체에 대해서는 단순히 close 메서드를 호출하여 리소스를 종료합니다.
+ * closeSockets 메서드는 Socket 배열에 대한 Socket 객체를 안전하게 종료하는 역할을 합니다.
+ * 각 socket 객체를 순회하면서 shutdownOutput 메서드를 호출하고, close 메서드를 호출하여 리소스를 종료합니다.
+ */
+
+/*
+ * EgovBindinginitializer.java 파일 요약
+ * EgovBindingInitializer 클래스는 WebBindingInitializer 인터페이스를 구현하여 Spring Web의 데이터
+ * 바인딩 초기화를 담당합니다.
+ * initBinder 메서드는 데이터 바인더 초기화를 위한 메서드입니다.
+ * 이 메서드에서는 날짜 형식을 지정하는 SimpleDateFormat 객체를 생성하고, 이를 이용하여 Date 타입에 대한 커스텀 에디터를
+ * 등록합니다.
+ * 또한, String 타입에 대한 커스텀 에디터를 등록하여 문자열의 앞뒤 공백을 제거합니다.
+ * 이 클래스는 데이터 바인딩 초기화를 담당하는데 사용되며, 웹 요청과 관련된 데이터를 적절한 형식으로 변환하여 컨트롤러에서 사용할 수 있도록
+ * 합니다.
+ */
+
+/*
+ * EgovMultipartResolver.java 파일 요약
+ * EgovMultipartResolver 클래스는 Spring의 CommonsMultipartResolver를 확장하여, 멀티파트 요청에서
+ * 파일 아이템을 파싱하는 역할을 합니다.
+ * parseFileItems 메서드는 FileItem 목록을 순회하면서 폼 필드와 파일 아이템을 구분하여 처리합니다.
+ * 폼 필드인 경우, 해당 필드의 값을 읽어서 multipartParameters 맵에 저장합니다.
+ * 파일 아이템인 경우, 해당 파일 아이템의 정보를 이용하여 CommonsMultipartFile 객체를 생성하고, multipartFiles
+ * 맵에 저장합니다.
+ * CommonsFileUploadSupport.MultipartParsingResult 객체를 반환하며, 이 객체에는 파싱된 파일 아이템과
+ * 폼 필드 정보가 저장되어 있습니다.
+ */
+
+/*
+ * EgovComExceptHndlr.java 파일 요약
+ * EgovComExcepHndlr 클래스는 ExceptionHandler 인터페이스를 구현하여 예외 처리를 담당합니다.
+ * 이 클래스는 예외가 발생했을 때 호출되는 메서드를 정의하고 있습니다.
+ * LOGGER는 로그를 기록하기 위한 Logger 객체입니다.
+ * LoggerFactory.getLogger 메서드를 사용하여 EgovComExcepHndlr 클래스에 대한 Logger 객체를 생성합니다.
+ * occur 메서드는 예외가 발생했을 때 호출되는 메서드입니다. 이 메서드는 발생한 예외와 패키지 이름을 로그로 기록합니다.
+ * 이 정보는 나중에 예외를 분석하거나 디버깅할 때 유용하게 사용할 수 있습니다.
+ */
+
+/*
+ * EgovComOtherExceptHndlr.java 파일 요약
+ * EgovComOthersExcepHndlr 클래스는 ExceptionHandler 인터페이스를 구현하여 예외 처리를 담당합니다.
+ * 이 클래스는 예외가 발생했을 때 호출되는 메서드를 정의하고 있습니다.
+ * LOGGER는 로그를 기록하기 위한 Logger 객체입니다.
+ * LoggerFactory.getLogger 메서드를 사용하여 EgovComOthersExcepHndlr 클래스에 대한 Logger 객체를
+ * 생성합니다.
+ * occur 메서드는 예외가 발생했을 때 호출되는 메서드입니다.
+ * 이 메서드는 발생한 예외와 패키지 이름을 로그로 기록합니다.
+ * 이 정보는 나중에 예외를 분석하거나 디버깅할 때 유용하게 사용할 수 있습니다.
+ */
+
+/*
+ * EgovMessageSource.java 파일 주요설명
+ * EgovMessageSource 클래스는 MessageSource 인터페이스와
+ * ReloadableResourceBundleMessageSource 클래스를 확장하여, 메시지 소스에서 메시지를 읽어오는 역할을 합니다.
+ * reloadableResourceBundleMessageSource는 ReloadableResourceBundleMessageSource
+ * 객체를 참조하는 필드입니다. 이 객체를 통해 메시지 소스에서 메시지를 읽어올 수 있습니다.
+ * setReloadableResourceBundleMessageSource 메서드는
+ * ReloadableResourceBundleMessageSource 객체를 설정하는 메서드입니다.
+ * getReloadableResourceBundleMessageSource 메서드는
+ * ReloadableResourceBundleMessageSource 객체를 반환하는 메서드입니다.
+ * getMessage 메서드는 주어진 코드에 해당하는 메시지를 반환하는 메서드입니다. 이 메서드는
+ * ReloadableResourceBundleMessageSource의 getMessage 메서드를 호출하여 메시지를 가져옵니다.
+ */
+
+/*
+ * EgovWebUtil.java 파일 요약
+ * EgovWebUtil 클래스는 웹 관련 유틸리티 메서드를 제공합니다. 이 클래스는 다양한 기능을 수행하여 웹 애플리케이션의 보안과 관련된
+ * 작업을 지원합니다.
+ * clearXSSMinimum 메서드는 최소한의 XSS 필터링을 수행합니다. 주어진 문자열에서 HTML 태그 및 특수 문자를 치환하여 XSS
+ * 공격을 방지합니다.
+ * clearXSSMaximum 메서드는 최대한의 XSS 필터링을 수행합니다. clearXSSMinimum 메서드를 호출하고 추가적으로 XSS
+ * 공격에 사용되는 특정 문자열을 필터링합니다.
+ * filePathBlackList 메서드는 파일 경로에 대한 블랙리스트 필터링을 수행합니다. 주어진 문자열에서 상위 디렉토리 접근을 시도하는
+ * 경로를 제거하여 보안을 강화합니다.
+ * filePathReplaceAll 메서드는 파일 경로에 대한 치환을 수행합니다. 주어진 문자열에서 파일 경로와 관련된 특정 문자를 제거하여
+ * 파일 경로의 유효성을 검사합니다.
+ * filePathWhiteList 메서드는 파일 경로에 대한 화이트리스트 필터링을 수행합니다. 주어진 문자열에 대한 추가적인 필터링 없이
+ * 그대로 반환합니다.
+ * isIPAddress 메서드는 주어진 문자열이 IP 주소인지 확인합니다. 정규 표현식을 사용하여 IP 주소 형식에 맞는지 검사합니다.
+ * removeCRLF 메서드는 주어진 문자열에서 개행 문자(CRLF)를 제거합니다.
+ * removeSQLInjectionRisk 메서드는 주어진 문자열에서 SQL 인젝션 가능성을 제거합니다. 주로 사용되는 SQL 인젝션 공격에
+ * 사용되는 특수 문자 및 기호를 제거합니다.
+ * removeOSCmdRisk 메서드는 주어진 문자열에서 OS 명령어 실행 가능성을 제거합니다. 주로 사용되는 OS 명령어 인젝션 공격에
+ * 사용되는 특수 문자 및 기호를 제거합니다.
+ */
+
+/*
+ * LoginVO.java 파일 요약
+ * LoginVO 클래스는 로그인 정보를 담는 Value Object(VO) 클래스로 사용될 수 있습니다.
+ * 클래스의 멤버 변수들은 로그인 정보를 나타내는 다양한 속성들을 가지고 있습니다.
+ * 각 속성에 대해 getter와 setter 메서드가 정의되어 있습니다.
+ * 이를 통해 속성 값을 설정하거나 가져올 수 있습니다.
+ * toString 메서드는 객체를 문자열로 변환하는 메서드로,
+ * ToStringBuilder 클래스를 사용하여 객체의 속성들을 문자열로 표현합니다.
+ * 이는 객체의 디버깅이나 로깅 등에 유용할 수 있습니다.
+ */
+
+/*
+ * CustomRequestMappingHandlerMapping.java 파일 요약
+ * CustomRequestMappingHandlerMapping 클래스는
+ * 스프링 MVC의 RequestMappingHandlerMapping을 확장하여 컨트롤러 인터셉터를 추가하는 기능을 제공합니다.
+ * getHandlerExecutionChain 메서드는 핸들러 실행 체인에 컨트롤러 인터셉터를 추가합니다.
+ * handler가 HandlerMethod인 경우에만 동작하며,
+ * addControllerInterceptors 메서드를 호출하여 컨트롤러 인터셉터를 추가합니다.
+ * addControllerInterceptors 메서드는 컨트롤러 클래스와 메서드에 선언된
+ * 
+ * @Interceptor 어노테이션을 확인하여 컨트롤러 인터셉터를 추가합니다.
+ * 클래스와 메서드의 @Interceptor 어노테이션에서 지정된 값을 가져와서 컨트롤러 인터셉터의 속성에 설정합니다.
+ * getControllerInterceptor 메서드는 nameList에 포함된 이름을 사용하여 컨트롤러 인터셉터를 가져옵니다.
+ * 스프링 애플리케이션 컨텍스트에서 해당 이름의 빈을 찾고,
+ * 존재하면 컨트롤러 인터셉터 객체를 생성하여 속성 값을 설정하고 리스트에 추가합니다.
+ * 컨트롤러 인터셉터가 추가되면 배열로 변환하여 반환하며, 추가할 인터셉터가 없는 경우 null을 반환합니다.
+ */
+
+/*
+ * LoggingAspect.java 파일 요약
+ * LoggingAspect 클래스는 AOP를 활용하여 메서드 실행 시간을 로깅하는 역할을 합니다.
+ * logger는 로깅을 위한 Logger 객체입니다. getLogger 메서드를 사용하여 현재 클래스에 대한 로거를 생성합니다.
+ * logging 메서드는 메서드 실행 시간을 로깅하는 메서드입니다.
+ * ProceedingJoinPoint 객체를 인자로 받아 메서드 실행 전후의 시간을 측정하여 로깅합니다.
+ * StopWatch 클래스를 사용하여 메서드 실행 시간을 측정합니다.
+ * start 메서드로 시간 측정을 시작하고, stop 메서드로 시간 측정을 종료합니다.
+ * joinPoint.proceed()를 호출하여 실제 메서드를 실행합니다.
+ * finally 블록에서 stopWatch를 멈추고, 메서드 이름과 총 실행 시간을 로깅합니다.
+ * Signature 객체를 사용하여 메서드 이름을 가져옵니다.
+ * 예외가 발생한 경우는 catch 블록에서 해당 예외를 다시 던집니다.
+ */
+
+/*
+ * CrossScriptingFilter.java 파일 요약
+ * CrossScriptingFilter 클래스는 필터(Filter) 역할을 수행하여
+ * 크로스 스크립팅(Cross-Site Scripting) 공격을 방지하는 기능을 제공합니다.
+ * init 메서드는 필터를 초기화하는 메서드입니다. FilterConfig 객체를 인자로 받고,
+ * 필요한 초기화 작업을 수행합니다.
+ * doFilter 메서드는 실제 필터링 작업을 수행하는 메서드입니다.
+ * excludeUrl 메서드를 호출하여 특정 URL을 제외하고 필터를 적용할지 여부를 확인하고, 필터링 작업을 수행합니다.
+ * excludeUrl 메서드는 요청한 URL이 특정 조건을 만족하는지 확인하여 필터를 적용할지 여부를 결정합니다.
+ * printHeaderUtils 메서드는 Header 편의 조회를 출력하는 메서드입니다.
+ * 요청의 Host, Accept-Language, cookie, Content 등의 정보를 출력합니다.
+ * printHeader 메서드는 Header 정보를 출력하는 메서드입니다. 요청에 포함된 Header의 이름과 값을 출력합니다.
+ * printStartLine 메서드는 Request-Line 정보를 출력하는 메서드입니다.
+ * 요청의 메서드, 프로토콜, URL, URI, QueryString 등의 정보를 출력합니다.
+ * destroy 메서드는 필터를 종료하는 메서드입니다. 필요한 정리 작업을 수행합니다.
+ * CrossScriptingFilter 클래스는 Filter 인터페이스를 구현하여 필터로 사용할 수 있도록 합니다.
+ */
